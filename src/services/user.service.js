0036a2ee9eb78e94579d21034c78ab7b021af296
thanglovemusic94 <thanglovemusic94@gmail.com>
2023-05-05 04:28:49 +0700

    fix

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import authHeader from "./auth-header";
 
 const API_URL = "http://aoerank.ap-southeast-1.elasticbeanstalk.com/api/auth/review";
-const API_URL2 = "http://aoerank.ap-southeast-1.elasticbeanstalk.com/api/home/all";
+const API_URL2= "http://aoerank.ap-southeast-1.elasticbeanstalk.com/api/home";
 
 const getAll = () => {
   return axios.get(API_URL2)
