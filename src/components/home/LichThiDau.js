dcd6ebfef8fc15b04eef106e79c72cb54686d0dc
thanglovemusic94 <thanglovemusic94@gmail.com>
2023-06-13 07:27:15 +0700

    no message

--- src/components/home/LichThiDau.js
+++ src/components/home/LichThiDau.js
@@ -66,7 +66,7 @@ const LichThiDau = () =>{
                                         <h5>13/06/2023</h5>
                                         <ul>
                                             <li className={'font-weight-bold'}>21h - TEAM 7 vs TEAM 8</li>
-                                            <li className={'font-weight-bold'}>8h30h - TEAM 3 vs TEAM 4</li>
+                                            <li className={'font-weight-bold'}>20h30h - TEAM 3 vs TEAM 4</li>
                                         </ul>
                                     </>
                                 },
